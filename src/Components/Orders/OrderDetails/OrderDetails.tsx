cf787e8e9d3afb840e251c1e681645a3559d5958
Daxxus <122165221+Daxxus@users.noreply.github.com>
2024-01-11 18:12:36 +0100

    Update OrderDetails.tsx

--- src/Components/Orders/OrderDetails/OrderDetails.tsx
+++ src/Components/Orders/OrderDetails/OrderDetails.tsx
@@ -23,7 +23,7 @@ const updateMealOrderById = async (
 	updateMealOrder: SingleMeal,
 	id: string | undefined
 ) => {
-	const response = await fetch(`http://localhost:3000/clientOrders/${id}`, {
+	const response = await fetch(`https://test-json-gamma.vercel.app/clientOrders/${id}`, {
 		method: "PUT",
 		headers: { "Content-type": "application/json;charset=UTF-8" },
 		body: JSON.stringify(updateMealOrder),
