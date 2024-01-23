26804ad50f481854e735d9a84ba7ff0e67f29032
Daxxus <122165221+Daxxus@users.noreply.github.com>
2024-01-23 10:28:50 +0100

    Update Reservation.tsx

--- src/Components/Orders/Reservation.tsx
+++ src/Components/Orders/Reservation.tsx
@@ -61,7 +61,7 @@ const Reservation = () => {
 		setDatetime(val)
 	}
 	const addReservation = async (newReservation: ReservationDetails) => {
-		await axios.post(`https://test-json-gamma.vercel.app`, newReservation)
+		await axios.post(`https://cleanserver.onrender.com`, newReservation)
 
 		onClose()
 	}
