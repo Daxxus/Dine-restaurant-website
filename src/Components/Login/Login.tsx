3ad629f86c0e97334578da78d01540f4016162d3
Daxxus <122165221+Daxxus@users.noreply.github.com>
2024-01-28 22:12:15 +0100

    Update Login.tsx

--- src/Components/Login/Login.tsx
+++ src/Components/Login/Login.tsx
@@ -54,7 +54,7 @@ const Login = () => {
 
 	const logUser = async (client: Login) => {
 		const resp = await axios.get(
-			`https://test-json-gamma.vercel.app/clients?email=${client.email}`
+			`https://mulberry-thirsty-hide.glitch.me/clients?email=${client.email}`
 		)
 
 		const { data } = resp
