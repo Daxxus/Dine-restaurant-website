181295afc3665ad5d732b98491300e847aca432f
Daxxus <122165221+Daxxus@users.noreply.github.com>
2024-02-15 10:48:57 +0100

    Update useReservations.tsx

--- src/Components/Clients/useReservations.tsx
+++ src/Components/Clients/useReservations.tsx
@@ -7,15 +7,9 @@ const useReservations = () => {
 	const { data: reservations, isLoading: loading } = useQuery({
 		queryKey: ["reservations", clientId],
 		queryFn: () =>
-<<<<<<< HEAD
 			fetch(
-				`http://localhost:3000/reservations?clientId=${clientId}`
+				`https://test-json-gamma.vercel.app/reservations?clientId=${clientId}`
 			).then((res) => res.json()),
-=======
-			fetch(`https://mulberry-thirsty-hide.glitch.me/reservations?clientId=${clientId}`).then(
-				(res) => res.json()
-			),
->>>>>>> 10c0d108e818b594454bd7433696e9a5fdacd759
 	})
 
 	return { reservations, loading }
